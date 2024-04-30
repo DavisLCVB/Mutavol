050ef7eb0684fb658a91b84941100a3e48870452
Davis LCVB <daviscvb22092004@gmail.com>
2024-04-30 02:52:29 -0500

    Creada clase scanner

---
+++ source/scanner/scanner.cpp
@@ -0,0 +1,10 @@
+#include "scanner.hpp"
+
+namespace mtv{
+    wchar_t Scanner::scan(){
+        if(index < size){
+        return buffer[index++];
+        }
+        return 0;
+    }
+}
--- source/scanner/scanner.hpp
+++ source/scanner/scanner.hpp
@@ -2,8 +2,19 @@
 #ifndef SCANNER_HPP
 #define SCANNER_HPP
 
-namespace mtv{
-    
-}
+#include <vector>
+
+namespace mtv {
+class Scanner {
+  public:
+    Scanner()  =default;
+    Scanner(const std::vector<wchar_t>& buffer): buffer(buffer) {};
+    wchar_t scan();
+  private:
+    std::vector<wchar_t> buffer;
+    size_t index{0};
+    size_t size{0};
+};
+} // namespace mtv
 
 #endif // SCANNER_HPP
