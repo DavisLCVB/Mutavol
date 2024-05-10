cca9d3332775d6cc98ce63404373f35e1d11dbd5
Davis <daviscvb22092004@gmail.com>
2024-05-10 18:31:57 -0500

    Eliminado scanner y files

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,4 @@
 #include "../source/args/args.hpp"
-#include "../source/files/files.hpp"
-#include "../source/scanner/scanner.hpp"
 #include <iostream>
 
 int main(int argc, char *argv[]) {
@@ -13,17 +11,4 @@ int main(int argc, char *argv[]) {
     std::cout << "Output file: " << output_file << "\n";
     auto input_file = args.get_input_file();
     std::cout << "Input file: " << input_file << "\n";
-    mtv::File file(input_file);
-    file.read();
-    auto tokens = file.get_tokens();
-    for (auto token : tokens) {
-        std::wcout << token;
-    }
-    std::wcout << std::endl;
-    mtv::Scanner scanner(tokens);
-    scanner.scan();
-    auto scanned_tokens = scanner.get_pre_tokens();
-    for (auto token : scanned_tokens) {
-        std::wcout << token << std::endl;
-    }
 }
