9d42077c5f189193cefab9314b9d9c7514b36f8d
flyreh <jose.manusc12224@gmail.com>
2024-05-20 04:10:09 -0500

    slicer

---
+++ tests/testslicer.cpp
@@ -0,0 +1,38 @@
+#include "../source/core/slicer/slicer.hpp"
+#include<iostream>
+#include<cassert>
+int main(const int argc, char *argv[]){
+
+    //false buffer ""
+   /* std::vector<char> test_buffer = {'i', 'n', 't', ' ', 'm', 'a', 'i', 'n', '(', 'c', 'o', 'n', 's', 't', ' ',
+                                     'i', 'n', 't', ' ', 'a', 'r', 'g', 'c', ',', ' ', 'c', 'h', 'a', 'r', ' ',
+                                     '*', 'a', 'r', 'g', 'v', '[', ']', ')', '{', '\n', ' ', ' ', 'm', 't', 'v',
+                                     ':', ':', 'A', 'r', 'g', 's', ' ', '&', 'a', 'r', 'g', 's', ' ', '=', ' ',
+                                     'm', 't', 'v', ':', ':', 'A', 'r', 'g', 's', ':', ':', 'g', 'e', 't', '_',
+                                     'i', 'n', 's', 't', 'a', 'n', 'c', 'e', '(', 'a', 'r', 'g', 'c', ',', ' ',
+                                     'a', 'r', 'g', 'v', ')', ';', '\n', ' ', ' ', 'a', 'r', 'g', 's', '.', 'r',
+                                     'e', 'c', 'o', 'n', '(', ')', ';', '\n', ' ', ' ', 'i', 'f', ' ', '(', 'a',
+                                     'r', 'g', 's', '.', 's', 'h', 'o', 'u', 'l', 'd', '_', 'e', 'x', 'i', 't', '(',
+                                     ')', ')', ' ', '{', '\n', ' ', ' ', ' ', ' ', 'r', 'e', 't', 'u', 'r', 'n',
+                                     ' ', '0', ';', '\n', ' ', ' ', '}', '\n', ' ', ' ', 'a', 'u', 't', 'o', ' ',
+                                     'o', 'u', 't', 'p', 'u', 't', '_', 'f', 'i', 'l', 'e', ' ', '=', ' ', 'a',
+                                     'r', 'g', 's', '.', 'g', 'e', 't', '_', 'o', 'u', 't', 'p', 'u'};*/
+    /*mtv::slicer &slicer = mtv::slicer::get_instance(test_buffer);
+
+    for(int i = 0; i < test_buffer.size(); i++){
+        slicer.getnext_token();
+    }
+    return 0;*/
+
+    // false buffer "se obtendrá de clase buffer"
+    std::string str = "entero a = 12;\nsi (a = 12){}\nsino{ float b = 0.1; \n far.get();}";
+    std::vector<char> test_buffer(str.begin(), str.end());
+
+    mtv::slicer &slicer = mtv::slicer::get_instance(test_buffer);
+
+    for(int i = 0; i < test_buffer.size(); i++){
+        slicer.getnext_token();
+    }
+
+    return 0;
+}
