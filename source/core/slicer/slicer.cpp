816931f583aa0f859b16741889a95a060f50ab84
flyreh <jose.manusc12224@gmail.com>
2024-05-20 04:45:48 -0500

    slicer

--- source/core/slicer/slicer.cpp
+++ source/core/slicer/slicer.cpp
@@ -1,29 +1,34 @@
 #include "slicer.hpp"
 
-namespace mtv {
+namespace mtv
+{
     const std::string slicer::operators = "\"\'#,;(){}[]=*/-+<>&|!~^%?:.";
 
-    slicer* slicer::instance = nullptr;
+    slicer *slicer::instance = nullptr;
 
-    slicer& slicer::get_instance(const std::vector<char>& test_buffer) {
+    slicer &slicer::get_instance(const std::vector<char> &test_buffer)
+    {
         if (!instance)
             instance = new slicer(test_buffer);
         return *instance;
     }
 
-    void slicer::slice() {
-        while ((*(this->charcurrent) == ' ' || *(this->charcurrent) == '\n' || *(this->charcurrent) == '\t') && this->charcurrent != this->charend)
-        {
+    void slicer::slice()
+    {
+        while ((*(this->charcurrent) == ' ' || *(this->charcurrent) == '\n' || *(this->charcurrent) == '\t') &&
+               this->charcurrent != this->charend) {
             this->charcurrent++;
         }
-        if(charconcatenate())
+        if (charconcatenate())
             return;
         if (is_number())
             return;
         if (is_operator())
             return;
     }
-    const std::string slicer::getnext_token() {
+
+    const std::string slicer::getnext_token()
+    {
         if (this->charcurrent == this->charend)
         {
             return "fin";
@@ -32,13 +37,14 @@ namespace mtv {
         return this->tokens.back();
     }
 
-    bool slicer::charconcatenate() {
+    bool slicer::charconcatenate()
+    {
         char c = *(this->charcurrent);
         if (c >= 'a' && c <= 'z' || c == '_' || c >= 'A' && c <= 'Z')
         {
             std::string token;
             while ((this->charcurrent) != charend && ((c >= 'a' && c <= 'z') ||
-                                              (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z') || c == '_'))
+                                                      (c >= '0' && c <= '9' || c >= 'A' && c <= 'Z') || c == '_'))
             {
                 token.push_back(c);
                 (this->charcurrent)++;
@@ -51,10 +57,11 @@ namespace mtv {
         }
         return false;
     }
-    bool slicer::is_number() {
+
+    bool slicer::is_number()
+    {
         char c = *(this->charcurrent);
-        if (c >= '0' && c <= '9')
-        {
+        if (c >= '0' && c <= '9') {
             std::string token;
             while ((this->charcurrent) != charend && (c >= '0' && c <= '9') || c == '.')
             {
@@ -69,14 +76,17 @@ namespace mtv {
         }
         return false;
     }
-    bool slicer::is_operator() {
+
+    bool slicer::is_operator()
+    {
         char c = *(this->charcurrent);
         if (operators.find(c) != std::string::npos)
         {
             std::string token;
             token.push_back(c);
             (this->charcurrent)++;
-            if((this->charcurrent) != charend){
+            if ((this->charcurrent) != charend)
+            {
                 std::string double_symbol_token = token;
                 double_symbol_token.push_back(*(this->charcurrent));
                 case_double_symbols(double_symbol_token, token);
@@ -86,15 +96,21 @@ namespace mtv {
         }
         return false;
     }
-    void slicer::case_double_symbols(std::string &double_symbol_token, std::string &token) {
+
+    void slicer::case_double_symbols(std::string &double_symbol_token, std::string &token)
+    {
         if ((this->charcurrent) == this->charend)
         {
             return;
         }
-        if (double_symbol_token == "&&" || double_symbol_token == "||" || double_symbol_token == "==" || double_symbol_token == "!=" ||
-            double_symbol_token == "<=" || double_symbol_token == ">=" || double_symbol_token == "++" || double_symbol_token == "--" ||
-            double_symbol_token == "+=" || double_symbol_token == "-=" || double_symbol_token == "*=" || double_symbol_token == "/=" ||
-            double_symbol_token == "%=" || double_symbol_token == "&=" || double_symbol_token == "|=" || double_symbol_token == "^=" ||
+        if (double_symbol_token == "&&" || double_symbol_token == "||" || double_symbol_token == "==" ||
+            double_symbol_token == "!=" ||
+            double_symbol_token == "<=" || double_symbol_token == ">=" || double_symbol_token == "++" ||
+            double_symbol_token == "--" ||
+            double_symbol_token == "+=" || double_symbol_token == "-=" || double_symbol_token == "*=" ||
+            double_symbol_token == "/=" ||
+            double_symbol_token == "%=" || double_symbol_token == "&=" || double_symbol_token == "|=" ||
+            double_symbol_token == "^=" ||
             double_symbol_token == "<<=" || double_symbol_token == ">>=")
         {
             token = double_symbol_token;
@@ -102,7 +118,8 @@ namespace mtv {
         }
     }
 
-    const std::vector<std::string> slicer::get_tokens() const {
+    const std::vector<std::string> slicer::get_tokens() const
+    {
         return tokens;
     }
 } // namespace mtv
