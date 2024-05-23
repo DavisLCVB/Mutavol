55191fb2dcb8f452931e3403e02f97de9df4437a
Davis <135272448+DavisLCVB@users.noreply.github.com>
2024-05-23 19:14:10 +0000

    Agregada documentacion al reader

--- source/core/reader/reader.hpp
+++ source/core/reader/reader.hpp
@@ -2,6 +2,13 @@
 #ifndef READER_HPP
 #define READER_HPP
 
+/**
+* @file reader.hpp
+* @author Davis Cartagena
+* @brief Class and methods to read the input file
+*/
+
+// Include section
 #include <memory>
 #include <string>
 #include <utility>
@@ -11,37 +18,76 @@ namespace mtv {
     // Using space
     using std::string;
 
-    // Using space
+    /**
+    * @class Reader
+    * @brief Class to read the input file
+    * @author Davis Cartagena
+    */
     class Reader {
     public:
 
+        /**
+        * @brief Gets the instance of the Reader class
+        * @return The instance of the Reader class
+        */
         static Reader &get_instance(const std::string &input_file);
 
+        // Deleted copy constructor
         Reader(const Reader &) = delete;
 
+        // Deleted copy assignment operator
         Reader &operator=(const Reader &) = delete;
 
+        /**
+        * @brief Method to read the input file
+        * @return true if the file was read successfully, false otherwise
+        */
         [[nodiscard]]
         bool read() const;
 
     private:
+        // string to store the input file
         string input_file;
+        // Pointer to the unique instance of the class
         static std::unique_ptr<Reader> instance;
 
+        /**
+        * @brief Method to verify if the file exists
+        * @return true if the file exists, false otherwise
+        */
         [[nodiscard]]
         bool verify() const;
 
+        /**
+        * @brief Method to read the input file
+        * @return true if the file was read successfully, false otherwise
+        */
         [[nodiscard]]
         bool read_file() const;
 
+        /**
+        * @brief Method to delete comments and empty lines of de buffer
+        */
         static void clean();
 
+        /**
+        * @brief Method to delete line comments and block comments
+        */
         static void remove_comments();
 
+        /**
+        * @brief Method to delete empty lines
+        */
         static void remove_lines();
 
+        /**
+        * Constructor for the class
+        */
         explicit Reader(string input_file) : input_file(std::move(input_file)) {}
 
+        /**
+        * Method to initialize the pointer to the unique instance of the class
+        */
         static void init_instance(const std::string &input_file);
     }; // class Reader
 } // namespace mtv
